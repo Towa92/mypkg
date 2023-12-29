286c33a634b3d608050fc1c2f7d84caedeb14a8a
Towa GD <towaakun@gmail.com>
2023-12-29 18:49:03 +0900

    try3

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,32 +1,41 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query
+from std_msgs.msg import Int16
 
-def main():
-    rclpy.init()
-    node = Node("listener")
-    client = node.create_client(Query, 'query') #service client make
-    while not client.wait_for_service(timeout_sec=1.0): #service wait
-        node.get_logger().info('待機中')
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" %vmsg.data)
 
-    req = Query.Request()
-    req.name = "アシド"
-    future = client.call_async(req)　#asynchronuos service call
+rclpy.init()
+node = Node("Listener")
+pub = node.create_subscription(Int16, "countup", cb, 10)
+rclpy.spin(node)
 
-    while rclpy.ok():
-        rclpy.spin_once(node) #call once and end
-        if future.done(): #if over
-            try:
-                response = future.result()
-            except:
-                node.get_logger().info('呼び出し失敗')
-            else: #else for not except time
-                node.get_logger().info("age: {}".format(response.age))
-
-            break
-
-    node.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
+#def main():
+#    rclpy.init()
+#    node = Node("listener")
+#    client = node.create_client(Query, 'query') #service client make
+#    while not client.wait_for_service(timeout_sec=1.0): #service wait
+#        node.get_logger().info('待機中')
+#
+#    req = Query.Request()
+#    req.name = "アシド"
+#    future = client.call_async(req)　#asynchronuos service call
+#
+#    while rclpy.ok():
+#        rclpy.spin_once(node) #call once and end
+#        if future.done(): #if over
+#            try:
+#                response = future.result()
+#            except:
+#                node.get_logger().info('呼び出し失敗')
+#            else: #else for not except time
+#                node.get_logger().info("age: {}".format(response.age))
+#
+#            break
+#
+#    node.destroy_node()
+#    rclpy.shutdown()
+#
+#if __name__ == '__main__':
     main()
